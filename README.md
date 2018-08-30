f6874deb15bebd2092dbc91174f119bd96479a9c
Sakol Assawasagool <koobitor@gmail.com>
2018-08-30 10:18:42 +0700

    add error response

--- README.md
+++ README.md
@@ -13,3 +13,72 @@ Promise
     console.log(result)
   })
 ```
+
+### Success Response
+```
+{
+  "tracking_id": "600000000000",
+  "sender": "Medvine Limited",
+  "recipient": "Sakol Assawasagool",
+  "consignee": "Sakol Assawasagool",
+  "agent": {
+    "pickup": {
+      "agent_name": "Bang Bua Thong Agency",
+      "agent_tel": "000-000-0000",
+      "dm": "Mr. Lorem Ipsum",
+      "dm_phone": "000-000-0000"
+    },
+    "delivery": {
+      "agent_name": "Bangna Agency",
+      "agent_tel": "00-000-0000",
+      "dm": "Mr. Lorem Ipsum",
+      "dm_phone": "000-000-0000"
+    }
+  },
+  "tracking": [
+    {
+      "date": "19\/04\/2018",
+      "time": "17:51:10",
+      "agent": "Bang Bua Thong Agency",
+      "scan_division": "Pickup Complete",
+      "etc": ""
+    },
+    {
+      "date": "19\/04\/2018",
+      "time": "18:30:09",
+      "agent": "Bang Bua Thong Agency",
+      "scan_division": "Pickup Start",
+      "etc": ""
+    },
+    {
+      "date": "20\/04\/2018",
+      "time": "14:12:51",
+      "agent": "Central CDC",
+      "scan_division": "Loading",
+      "etc": ""
+    },
+    {
+      "date": "21\/04\/2018",
+      "time": "08:06:28",
+      "agent": "Bangna Agency",
+      "scan_division": "Delivery Start",
+      "etc": ""
+    },
+    {
+      "date": "23\/04\/2018",
+      "time": "19:00:33",
+      "agent": "Bangna Agency",
+      "scan_division": "Delivery Complete",
+      "etc": ""
+    }
+  ]
+}
+```
+
+### Error Response
+```
+{
+  error: 'invalid_order_no',
+  msg: 'Invalid Order No.!'
+}
+```
--- index.js
+++ index.js
@@ -14,72 +14,81 @@ exports.cj = async (id) => {
 
   const $ = await cheerio.load(result[0])
 
-  // tracking_id, status
-  const bill = $('table').eq(0).text().trim().replace('Bill No. : ','').split('(')
-  const tracking_id = bill[0].trim()
-  const status = bill[1].replace(')','')
+  const check = $('table').eq(0).text().trim()
 
-  // sender, recipient, consignee
-  const basic = $('table').eq(2).find('tr').eq(1)
-  const sender = basic.find('td').eq(0).text().trim()
-  const recipient = basic.find('td').eq(1).text().trim()
-  const consignee = basic.find('td').eq(3).text().trim()
+  if(check != ""){
+    // tracking_id, status
+    const bill = $('table').eq(0).text().trim().replace('Bill No. : ','').split('(')
+    const tracking_id = bill[0].trim()
+    const status = bill[1].replace(')','')
 
-  // agent
-  const agent = $('table').eq(4).find('tr')
-  const pickup = agent.eq(1)
-  const delivery = agent.eq(2)
-  const pickup_agent_name = pickup.find('td').eq(1).text().trim()
-  const pickup_agent_tel = pickup.find('td').eq(2).text().trim()
-  const pickup_dm = pickup.find('td').eq(3).text().trim()
-  const pickup_dm_phone = pickup.find('td').eq(4).text().trim()
-  const delivery_agent_name = delivery.find('td').eq(1).text().trim()
-  const delivery_agent_tel = delivery.find('td').eq(2).text().trim()
-  const delivery_dm = delivery.find('td').eq(3).text().trim()
-  const delivery_dm_phone = delivery.find('td').eq(4).text().trim()
+    // sender, recipient, consignee
+    const basic = $('table').eq(2).find('tr').eq(1)
+    const sender = basic.find('td').eq(0).text().trim()
+    const recipient = basic.find('td').eq(1).text().trim()
+    const consignee = basic.find('td').eq(3).text().trim()
 
-  // tracking
-  let trackings = []
-  const tracking = $('table').eq(6).find('tr')
-  tracking.each((i,item) => {
-    if(i > 0){
-      const row = $(item).find('td')
-      const date = row.eq(0).text().trim()
-      const time = row.eq(1).text().trim()
-      const agent = row.eq(2).text().trim()
-      const scan_division = row.eq(3).text().trim()
-      const etc = row.eq(4).text().trim()
+    // agent
+    const agent = $('table').eq(4).find('tr')
+    const pickup = agent.eq(1)
+    const delivery = agent.eq(2)
+    const pickup_agent_name = pickup.find('td').eq(1).text().trim()
+    const pickup_agent_tel = pickup.find('td').eq(2).text().trim()
+    const pickup_dm = pickup.find('td').eq(3).text().trim()
+    const pickup_dm_phone = pickup.find('td').eq(4).text().trim()
+    const delivery_agent_name = delivery.find('td').eq(1).text().trim()
+    const delivery_agent_tel = delivery.find('td').eq(2).text().trim()
+    const delivery_dm = delivery.find('td').eq(3).text().trim()
+    const delivery_dm_phone = delivery.find('td').eq(4).text().trim()
 
-      trackings.push({
-        "date": date,
-        "time": time,
-        "agent": agent,
-        "scan_division": scan_division,
-        "etc": etc
-      })
-    }
-  })
+    // tracking
+    let trackings = []
+    const tracking = $('table').eq(6).find('tr')
+    tracking.each((i,item) => {
+      if(i > 0){
+        const row = $(item).find('td')
+        const date = row.eq(0).text().trim()
+        const time = row.eq(1).text().trim()
+        const agent = row.eq(2).text().trim()
+        const scan_division = row.eq(3).text().trim()
+        const etc = row.eq(4).text().trim()
 
-  return {
-    "tracking_id": tracking_id,
-    "status": status,
-    "sender": sender,
-    "recipient": recipient,
-    "consignee": consignee,
-    "agent": {
-      "pickup": {
-        "agent_name": pickup_agent_name,
-        "agent_tel": pickup_agent_tel,
-        "dm": pickup_dm,
-        "dm_phone": pickup_dm_phone
-      },
-      "delivery": {
-        "agent_name": delivery_agent_name,
-        "agent_tel": delivery_agent_tel,
-        "dm": delivery_dm,
-        "dm_phone": delivery_dm_phone
+        trackings.push({
+          "date": date,
+          "time": time,
+          "agent": agent,
+          "scan_division": scan_division,
+          "etc": etc
+        })
       }
-    },
-    "tracking": trackings
+    })
+
+    return {
+      "tracking_id": tracking_id,
+      "status": status,
+      "sender": sender,
+      "recipient": recipient,
+      "consignee": consignee,
+      "agent": {
+        "pickup": {
+          "agent_name": pickup_agent_name,
+          "agent_tel": pickup_agent_tel,
+          "dm": pickup_dm,
+          "dm_phone": pickup_dm_phone
+        },
+        "delivery": {
+          "agent_name": delivery_agent_name,
+          "agent_tel": delivery_agent_tel,
+          "dm": delivery_dm,
+          "dm_phone": delivery_dm_phone
+        }
+      },
+      "tracking": trackings
+    }
+  }else{
+    return {
+      "error": "invalid_order_no",
+      "msg": "Invalid Order No.!"
+    }
   }
 }
